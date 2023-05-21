e1f0c9de821d32ea20c316aeccf9148f18573382
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2023-05-22 00:50:53 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,36 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.jetbrains.annotations.NotNull;
 
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @NotNull
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+        /*
+           나중에 DBMemberRepository 등 다른 구현체로 변경해야 할 때
+           이 부분만 변경하면 되기 떄문에 편리하다.
+           아래 discountPolicy도 마찬가지
+         */
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
