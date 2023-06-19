e1768760019f527ae22c3d4a631db5be3dca7ecc
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2023-06-19 22:56:34 +0900

    MemberApp 파일 Spring으로 전환

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MemberApp {
     public static void main(String[] args) {
+
         // AppConfig appConfig = new AppConfig();
         // MemberService memberService = appConfig.memberService();
         // MemberService memberService = new MemberServiceImpl();
