be74e07c135c9931a76258b41912460a5c293c7d
hyewo-n <95971431+hyewo-n@users.noreply.github.com>
2023-10-26 23:29:25 +0900

    AppConfig에 호출 로그 남겨서 확인

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,11 +17,15 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        // 1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
         /*
            나중에 DBMemberRepository 등 다른 구현체로 변경해야 할 때
@@ -32,6 +36,8 @@ public class AppConfig {
 
     @Bean
     public OrderService orderService() {
+        // 1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
